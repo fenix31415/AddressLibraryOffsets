c1e7120f35a3f719a3433979bb96f850c4f2ae49
fenix31415 <dimkakirov43@mail.ru>
2023-06-16 12:20:28 +0300

    Init

---
+++ Main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+//#define AE  // uncomment it for AE, comment it for SSE
+
+#ifdef AE
+
+#include "versionlibdb.h"
+
+bool DumpSpecificVersion()
+{
+	VersionDb db;
+
+	if (!db.Load(1, 6, 640, 0)) {
+		std::cout << "-\n";
+		return false;
+	}
+
+	db.Dump("offsets-1-6-659-0");
+	return true;
+}
+
+#else
+
+#include "versiondb.h"
+
+bool DumpSpecificVersion()
+{
+	VersionDb db;
+
+	if (!db.Load(1, 5, 97, 0)) {
+		std::cout << "-\n";
+		return false;
+	}
+
+	db.Dump("offsets-1-5-97-0.txt");
+	return true;
+}
+
+#endif
+
+int main() {
+	DumpSpecificVersion();
+}
