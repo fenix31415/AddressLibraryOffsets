7ecbf77aa0014c8974a551704f5296c4dfae6f1b
fenix31415 <dimkakirov43@mail.ru>
2023-06-23 13:16:44 +0300

    rename ae file

--- Main.cpp
+++ Main.cpp
@@ -10,12 +10,12 @@ bool DumpSpecificVersion()
 {
 	VersionDb db;
 
-	if (!db.Load(1, 6, 640, 0)) {
+	if (!db.Load(1, 6, 318, 0)) {
 		std::cout << "-\n";
 		return false;
 	}
 
-	db.Dump("offsets-1-6-659-0");
+	db.Dump("offsets-1-6-318-0.txt");
 	return true;
 }
 
